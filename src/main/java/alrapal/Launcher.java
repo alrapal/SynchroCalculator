0a71750cc79cfca7b2c0d6caef1af8359bee48d7
Alexandre Rancati-Palmer <71485862+alrapal@users.noreply.github.com>
2021-03-23 13:40:56 +0100

    Licensing

--- src/main/java/alrapal/Launcher.java
+++ src/main/java/alrapal/Launcher.java
@@ -1,5 +1,36 @@
 package alrapal;
 
+/** Synchro Calculator is a calculator that aims to help Xelor players from the game Dofus (https://www.dofus.com)
+ * to keep track of the evolution of the spell "Synchro" as well as anticipate the damages it will cause on a given adversary.
+ *
+ * Developed by: Alexandre Rancati-Palmer
+ * Contact: alexandrerancatipalmer@gmail.com
+ *          https://www.linkedin.com/in/alexandre-rancati-palmer/
+ *
+ * License:
+ *      MIT License
+ *
+ * Copyright (c) 2021 Alexandre Rancati-Palmer
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "SynchroCalculator.txt"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.**/
+
+
 public class Launcher {
     public static void main(String[] args) {
         App.main(args);
