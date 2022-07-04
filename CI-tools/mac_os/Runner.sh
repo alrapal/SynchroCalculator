61c2b4712a622225568877cd68b9e3c4ab90a985
Alexandre Rancati-Palmer <a.rancatipalmer@mailo.com>
2022-07-04 19:24:56 +0200

    Bundle of the app for MacOS
    - Several scripts to bundle the app as an .app file for mac os
    
    linked issue #12

---
+++ CI-tools/mac_os/Runner.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Inspired by https://cuneyt.aliustaoglu.biz/en/creating-standalone-macos-app-dmg-from-javafx/
+
+cd -- "$(dirname -- "$BASH_SOURCE")"
+cd bin
+./SynchroLauncher
+exit 0
