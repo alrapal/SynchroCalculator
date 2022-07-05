42fcfb2d8e4957790dcd181d3fadca66ee15d5db
Alexandre Rancati-Palmer <a.rancatipalmer@mailo.com>
2022-07-05 09:43:42 +0200

    Archiving the app for distribution
    - The app is now zipped with a name that mention the architecture of
      supported for better distribution
    
    related issue #12

--- CI-tools/mac_os/bundle.sh
+++ CI-tools/mac_os/bundle.sh
@@ -1,8 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # Inspired by https://cuneyt.aliustaoglu.biz/en/creating-standalone-macos-app-dmg-from-javafx/
 
 #Variables
 NAME="Synchro Calculator"
+ARCHITECTURE=$HOSTTYPE
 ICON_PATH="../CI-tools/general/logo.icns"
 RUNNER_PATH="../CI-tools/mac_os/Runner.sh"
 BUNDLE_DIR_NAME=bundle_macOS
@@ -20,3 +21,5 @@ chmod +x "$RUNNER_PATH"
 $APPIFY_PATH -i "$ICON_PATH" -n "$NAME" -s "$RUNNER_PATH"
 # Copies the image generated with javafx:jlink plugin in the bundle
 cp -R "$JLINK_IMAGE_PATH" "$BUNDLE_PATH"
+# compress the image for distribution efficiency
+zip "${NAME}_${ARCHITECTURE}.zip" -r "${NAME}.app"
