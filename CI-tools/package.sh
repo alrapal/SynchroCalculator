277cda4f64e8090a6b2d0f2b2d5ba92c266330e6
Alexandre Rancati-Palmer <a.rancatipalmer@mailo.com>
2022-12-31 16:14:39 +0100

    Creation of 2 separated workflows + version control based on tag creation #12

--- CI-tools/package.sh
+++ CI-tools/package.sh
@@ -2,6 +2,7 @@
 
 # Path for the icon is given as argument when calling the script (different format based on OS)
 ICON_PATH=$1
+VERSION=$2
 
 jpackage \
  --name "Synchro Calculator" \
@@ -10,4 +11,9 @@ jpackage \
  --main-class alrapal.App \
  --runtime-image target/SynchroCalculator \
  --java-options "--add-exports javafx.base/com.sun.javafx.event=org.controlsfx.controls" \
- --icon "$ICON_PATH"
+ --icon "$ICON_PATH" \
+ --vendor "Alexandre Rancati-Palmer" \
+ --copyright "Code: Copyright (c) 2022, Alexandre Rancati-Palmer
+ a.rancatipalmer@mailo.com
+ Pictures: Copyright (c) 2022, Ankama Games" \
+ --app-version "$VERSION"
