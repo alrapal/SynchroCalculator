cf04999aafcfe80beeafabf531fbf4129039a7d0
Alexandre Rancati-Palmer <a.rancatipalmer@mailo.com>
2023-07-08 12:10:49 +0200

    Use Version Plugin to set version in pom file using CI Use gh-release action to store.dmg as release asset

--- CI-tools/package.sh
+++ CI-tools/package.sh
@@ -13,7 +13,7 @@ jpackage \
  --java-options "--add-exports javafx.base/com.sun.javafx.event=org.controlsfx.controls" \
  --icon "$ICON_PATH" \
  --vendor "Alexandre Rancati-Palmer" \
- --copyright "Code: Copyright (c) 2022, Alexandre Rancati-Palmer
+ --copyright "Code: Copyright (c) 2023, Alexandre Rancati-Palmer
  a.rancatipalmer@mailo.com
- Pictures: Copyright (c) 2022, Ankama Games" \
+ Pictures: Copyright (c) 2023, Ankama Games" \
  --app-version "$VERSION"
